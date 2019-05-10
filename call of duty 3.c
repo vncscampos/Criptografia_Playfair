4b42bebdf75a58655904c121f3401c47b4f9e3e7
vncscampos <49920527+vncscampos@users.noreply.github.com>
2019-05-10 16:51:38 -0300

    Update and rename code3.c to call of duty 3.c

--- call of duty 3.c
+++ call of duty 3.c
@@ -1,24 +1,23 @@
-# #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
-#include <ctype.h> //pacote pra usar a função q deixa as letras maiúsculas
+#include <ctype.h>
+#include <locale.h>
 
-char** gerarTabela(char carac[], int *tamanho) { //int tamanho é um ponteiro que recebe endereço de um int la no main
+char** gerarTabela(char carac[], int *size) {
     int tam = strlen(carac);
-    for (int i = 0; i < tam; i++) { //embaralhar o vetor
+    for (int i = 0; i < tam; i++) { //embaralha o vetor
         int r = rand() % tam;
         char aux = carac[i];
         carac[i] = carac[r];
         carac[r] = aux;
     }
+
     int k = 0;
     int tam2 = 6;
 
-
-    //Malloc = aloca espaços para blocos de bytes consecutivos na memoria
-    //Sizeof = retorna a quantidade de bytes um objeto possui
     char **m = (char**) malloc(tam2 * sizeof (char*)); //Como se estivesse fazendo um char m[tam2]
     for (int i = 0; i < tam2; i++) { //Percorre as linhas do Vetor de Ponteiros
         m[i] = (char*) malloc(tam2 * sizeof (char)); //Aloca um Vetor de Inteiros para cada posição do Vetor de Ponteiros.
@@ -28,7 +27,7 @@ char** gerarTabela(char carac[], int *tamanho) { //int tamanho é um ponteiro qu
         }
     }
 
-    *tamanho = tam2; o valor do tam2 eu vou apontar praquele int no main
+    *size = tam2; //pega o tamanho da matriz
 
     return m;
 }
@@ -42,12 +41,20 @@ void imprimirTabela(char **matriz, int tam) {
     }
 }
 
-char* xInsertion(char msg[]) {
+char* xInsertion(char msg[], int space[]) {
     int pos = 0;
     int tam = strlen(msg);
     char *aux = (char*) malloc(tam * sizeof (char));
-    //retira os espaços em branco pra poder ficar mais facil a inserção
-    for (int j = 0; j < tam; j++) {
+
+    int k = 0;
+    for (int i = 0; i < tam; i++) { //salva os espaços da mensagem
+        if (msg[i] == ' ') {
+            space[k] = i;
+            k++;
+        }
+    }
+
+    for (int j = 0; j < tam; j++) { //remove os espaços da mensagem
         if (msg[j] == ' ') {
             for (int n = j; n < tam; n++) {
                 msg[n] = msg[n + 1];
@@ -55,7 +62,7 @@ char* xInsertion(char msg[]) {
         }
     }
 
-    for (int i = 0; i < tam;) {
+    for (int i = 0; i < tam;) { //insere o X
         if (msg[i] == msg[i + 1]) {
             aux[pos] = msg[i];
             aux[pos + 1] = 'X';
@@ -67,48 +74,49 @@ char* xInsertion(char msg[]) {
         }
         pos = pos + 2;
     }
-    int aux_size = strlen(aux);
+    int aux_size = strlen(aux) - 1;
     //verifica se a string é impar 
     //se sim, adiciona um x na ultima posição
-    if ((tam - 2) % 2 != 0) {
-        aux[aux_size - 1] = 'X';
+    if (aux_size % 2 != 0) {
+        aux[aux_size] = 'X';
     }
+
     return aux;
 }
 
-char* criptografar(char *msg, char **tab) {
+char* criptografar(char *msg, char **tab, int tam) {
     int pos = 0;
     int l1, l2, c1, c2;
-    while (pos < strlen(msg) - 2) {
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                if (tab[i][j] == msg[pos]) { //Econtra a primeira letra
+    while (pos < strlen(msg) - 1) {
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                if (tab[i][j] == msg[pos]) { //Encontra a primeira letra
                     l1 = i;
                     c1 = j;
-                } else if (tab[i][j] == msg[pos + 1]) {
+                } else if (tab[i][j] == msg[pos + 1]) { //encontra a segunda letra
                     l2 = i;
                     c2 = j;
                 }
             }
         }
         if (l1 == l2) {//Mesma linha
-            if (c1 + 1 < 6) {
+            if (c1 + 1 < tam) {
                 msg[pos] = tab[l1][c1 + 1];
             } else {
                 msg[pos] = tab[l1][0];
             }
-            if (c2 + 1 < 6) {
+            if (c2 + 1 < tam) {
                 msg[pos + 1] = tab[l2][c2 + 1];
             } else {
                 msg[pos + 1] = tab[l2][0];
             }
         } else if (c1 == c2) {//Mesma coluna
-            if (l1 + 1 < 6) {
+            if (l1 + 1 < tam) {
                 msg[pos] = tab[l1 + 1][c1];
             } else {
                 msg[pos] = tab[0][c1];
             }
-            if (l2 + 1 < 6) {
+            if (l2 + 1 < tam) {
                 msg[pos + 1] = tab[l2 + 1][c1];
             } else {
                 msg[pos + 1] = tab[0][c1];
@@ -122,13 +130,13 @@ char* criptografar(char *msg, char **tab) {
     return msg;
 }
 
-char* decrip(char *msg, char **tab) {
+char* decrip(char *msg, char **tab, int tam) {
     int pos = 0;
     int l1, l2, c1, c2;
-    while (pos < strlen(msg) - 2) {
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                if (tab[i][j] == msg[pos]) { //Econtra a primeira letra
+    while (pos < strlen(msg) - 1) {
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                if (tab[i][j] == msg[pos]) { //Encontra a primeira letra
                     l1 = i;
                     c1 = j;
                 } else if (tab[i][j] == msg[pos + 1]) { //Encontra a segunda letra
@@ -170,76 +178,125 @@ char* decrip(char *msg, char **tab) {
 }
 
 int main(int argc, char** argv) {
+    setlocale(LC_ALL, "");
     int acao, tam;
     char msgm[100];
     char *p_ch;
-    char **p;
+    int *space = (int*) calloc(100, sizeof (int));
     char carac[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
         'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char **p;
     char carac2[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N', 'O', 'P', 'Q',
-                        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
     bool verify = false; //Verifica a tabela 
     bool verify_crip = false; //Verifica a criptografia - Olhar o case 2
     bool verify_decrip = false; //Verifica a decriptografia - Olhar o case 3
-
     do {
         printf("\nDigite a ação que deseja fazer:\n");
         printf("1. Gerar tabela;\n");
         printf("2. Introduzir mensagem;\n");
         printf("3. Ver mensagem criptografada;\n");
         printf("4. Decriptografar mensagem;\n");
-        printf("5. Editar tabela;\n");
+        printf("5. Remover número;\n");
         printf("6. Imprimir tabela;\n");
         printf("7. SAIR.\n");
         printf("_________________________\n");
         scanf("%d", &acao);
         int ch;
         while ((ch = getchar()) != '\n' && ch != EOF);
+
         switch (acao) {
             case 1:
                 if (verify == false) { //n permite criar outra tabela principal
-                    p = gerarTabela(carac, &tam); //aqui to passando o endereço do int tam
+                    p = gerarTabela(carac, &tam);
                     verify = true;
                 }
                 break;
+
             case 2:
-                printf("Digite a mensagem: ");
+                printf("Digite a mensagem:");
                 fgets(msgm, 100, stdin);
                 for (int i = 0; i < strlen(msgm); i++) {
                     msgm[i] = toupper(msgm[i]); //transforma letras para maiúsculas
                 }
+
+
+
+                char vogal[] = {'A', 'A', 'A', 'A', 'E', 'E', 'I', 'O', 'O', 'U'};
+                int vogal_Acento[] = {160, 133, 198, 131, 130, 136, 161, 162, 147, 163};
+
+                
+                for (int i = 0; i < strlen(msgm); i++) {
+                    for (int j = 0; j < sizeof (vogal_Acento); j++) {
+                        int x = msgm[i];
+                        if (x == vogal_Acento[j]) {
+                            msgm[i] = vogal[j];
+                        }
+                    }
+                }
+
+                printf("%s", msgm);
+
                 verify_crip = true;
                 break;
+
             case 3:
                 if (verify_crip == true) { //n permite criptografar a mesma mensagem mais de uma vez
-                    p_ch = criptografar(p_ch, p);
+                    p_ch = xInsertion(msgm, space);
+                    p_ch = criptografar(p_ch, p, tam);
                     verify_decrip = true;
                 }
                 verify_crip = false;
 
-                for (int i = 0; i < strlen(p_ch); i++) { //imprime a criptografia dividindo
+                for (int i = 0; i < strlen(p_ch); i++) { //separa as letras de 4 em 4
                     if (i % 5 == 0) {
                         printf(" ");
                     } else {
                         printf("%c", p_ch[i]);
                     }
                 }
-
                 break;
+
             case 4:
                 if (verify_decrip == true) { //n permite decriptografar a mesma mensagem mais de uma vez
-                    p_ch = decrip(p_ch, p);
+                    p_ch = decrip(p_ch, p, tam);
                 }
+
+                for (int i = 0; i < strlen(p_ch); i++) { // retira o X
+                    if (p_ch[i] == 'X') {
+                        if (p_ch[i - 1] == p_ch[i + 1]) {
+                            p_ch[i] = ' ';
+                        }
+                        if (i == strlen(p_ch) - 1);
+                    }
+                }
+
+
+                for (int i = 0; i < sizeof (space); i++) { //coloca os espaços
+                    int aux = space[i];
+                    if (aux != 0) {
+                        for (int j = 99; j > aux; j--) {
+                            p_ch[j] = p_ch[j - 1];
+                        }
+                        p_ch[aux] = ' ';
+                    }
+                }
+
+
+
                 printf("%s\n", p_ch);
                 verify_decrip = false;
                 break;
+
             case 5:
-                p = gerarTabela(carac2, &tam); //aqui to passando o endereço do int tam
-         
+                p = gerarTabela(carac2, &tam);
                 break;
+
             case 6:
                 imprimirTabela(p, tam);
                 break;
+
         }
     } while (acao != 7);
 
