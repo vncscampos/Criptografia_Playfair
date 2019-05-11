452da71c27cd003c0c8f61c227958e382debd1be
vncscampos <49920527+vncscampos@users.noreply.github.com>
2019-05-11 17:48:14 -0300

    Update and rename code5 to code5.c

--- code5.c
+++ code5.c
@@ -273,7 +273,11 @@ int main(int argc, char** argv) {
                         }
                     }
                 }
-
+                
+                int aux_size = strlen(p_ch) - 1; //retira o X da ultima posiçao caso a palavra seja ímpar
+                if (aux_size % 2 != 0) {
+                    p_ch[aux_size] = p_ch[aux_size+1];
+                }
 
                 for (int i = 0; i < sizeof (space); i++) { //coloca os espaços
                     int aux = space[i];
